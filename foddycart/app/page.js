c76154b836f28a187dfb0cfbb34c202dd3628dc0
Kajal Singh <kajals.ug23.ec@nitp.ac.in>
2025-03-25 18:00:51 +0530

    initial commit

---
+++ foddycart/app/page.js
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+
+export default function Home() {
+  return (
+    <div>
+      <h2>subscribe</h2>
+<button>subscribe</button>
+    </div>
+  );
+}
---
+++ foddycart/lib/utils.js
@@ -0,0 +1,6 @@
+cd import { clsx } from "clsx";
+import { twMerge } from "tailwind-merge"
+
+export function cn(...inputs) {
+  return twMerge(clsx(inputs));
+}
